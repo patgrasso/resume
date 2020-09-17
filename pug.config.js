3b7540a61cd695e9d3d3067dded33bb9bc788a72
Patrick Grasso <patgra123@gmail.com>
2020-09-17 16:00:52 -0400

    feat: reimplement resume with parcel + pug
    Use pug templating to implement my resume! Fill in the blanks with a
    pug.config.js file.
    
    Note that `contact-info.json` holds my contact details, but is
    gitignore'd for my privacy. This file is imported by `pug.config.js`.

---
+++ pug.config.js
@@ -0,0 +1,94 @@
+const contact = require('./contact-info.json');
+
+module.exports = {
+  locals: {
+    contact,
+    education: [
+      {
+        school: "Stevens Institute of Technology",
+        graduationDate: "May 2018",
+        degree: "M.S. in Cybersecurity",
+        gpa: 3.67,
+        roles: [],
+      },
+      {
+        school: "Stevens Institute of Technology",
+        graduationDate: "May 2017",
+        degree: "B.S. in Computer Science and Mathematics Minor",
+        gpa: 3.40,
+        roles: [
+          {
+            title: "Teaching Assistant",
+            startDate: "February 2015",
+            endDate: "December 2017",
+            description: "Algorithms, Concurrent Programming, Operating Systems, Team Prog. & Prob. Solving",
+          },
+        ],
+      },
+    ],
+    experience: [
+      {
+        company: "IBM",
+        roles: [
+          {
+            title: "Software Engineer - TradeLens",
+            startDate: "August 2018",
+            description: [
+              "Collaborated with Maersk to build an event processing system capable of tracking the state of shipments and intermodal shipping containers with information provided by several large ocean transport companies.",
+              "Designed and implemented a generic, role-based, hierarchical access control system inspired by AWS IAM, Google Cloud IAM, and Open Policy Agent in order to enforce complex data access rules for TradeLens.",
+              // TODO: convey that I worked on almost all parts of the product
+              // and got a good understanding of problem domain as a result
+              //
+              // TODO: add something about researching EDIFACT and shipping
+              // domain
+              "Worked with stakeholders to design and develop a notification system built with Twilio and Sendgrid.",
+              "Used Kubernetes and Helm to orchestrate application deployments.",
+              "Participated in IBM's Golang developer community and led team education sessions on Golang.",
+            ],
+          },
+          {
+            title: "Software Engineering Intern - Extreme Blue",
+            startDate: "May",
+            endDate: "August 2017",
+            description: [
+              "Architected and implemented an identity management solution that uses a combination of biometrics and other factors to securely authenticate individuals.",
+              "Created and delivered a pitch to over 50 executives at IBM's corporate headquarters in Armonk, NY."
+            ],
+          },
+        ],
+      },
+      {
+        company: "iCIMS",
+        roles: [
+          {
+            title: "Software Developer Intern",
+            startDate: "June",
+            endDate: "August 2015",
+            description: [
+              "Designed and implemented a system to automate merges, saving each developer an average of 4 hours/week.",
+              "Created a user-friendly web interface for the system using EmberJS and Material Design Lite.",
+            ],
+          },
+          {
+            title: "Software Development Engineer in Test (SDET) Intern",
+            startDate: "June",
+            endDate: "August 2014",
+            description: [
+              "Implemented a system which aligns automation tests, unit tests, and QA tests within a test case management suite, saving QA technicians hours of manually entering test case information.",
+            ],
+          },
+        ],
+      },
+    ],
+    projects: [
+      {
+        name: "TaylorFit",
+        description: "Worked closely with a client and 3 other students to build an isomorphic web app for analyzing data using multivariate polynomial regression.",
+      },
+      {
+        name: "SemNet",
+        description: "Worked closely with a client and 3 other students to build an isomorphic web app for analyzing data using multivariate polynomial regression.",
+      },
+    ],
+  },
+};
